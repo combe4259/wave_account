195c35b419b78f902490fe1df17700e0e74b4053
han97901 <han97901@gmail.com>
2025-07-04 12:34:57 +0900

    init

---
+++ app/src/main/java/com/example/accountbook/model/Expense.kt
@@ -0,0 +1,16 @@
+package com.example.accountbook.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "expenses")
+data class Expense(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0L,
+
+    val productName: String,     // 상품명
+    val amount: Double,          // 금액
+    val category: String,        // 카테고리
+    val date: Long,              // 날짜
+    val photoUri: String? = null // 이미지 URI
+)
